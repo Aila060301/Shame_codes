769cb11ff17c687c6fa9eeeb2fba5d1660e63da6
Aila060301 <2768978819@qq.com>
2025-02-24 07:56:27 +0800

    Create kill_stupid_love.cpp

---
+++ kill_stupid_love.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main (){
+  int days=1;
+  while(days<=7){
+    cout <<" 已清除舔狗人格"<<days*14.28<<"%"<<endl;
+    days++;
+  }
+  cout <<"已完成今天的进度"<<endl;
+  return 0;
+}
